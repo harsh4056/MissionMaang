1ca9dfc843e3686c014b92a53f7d6151e4ae822f
Harsh Kumar Singh <hs4056@gmail.com>
2025-05-21 16:51:41 +0530

    questions

--- src/TextJustification.java
+++ src/TextJustification.java
@@ -7,14 +7,42 @@ public class TextJustification {
             int wordsIdx=0;
             List<String> list= new ArrayList<>();
         while(wordsIdx<words.length){
-            List<String >  data= new ArrayList<>();
+            int length=0;
             int width=maxWidth;
-            while(width>0){
-                width=width-words[wordsIdx].length();
-                data.add(words[wordsIdx]);
-                wordsIdx++;
+            List<String> temp=new ArrayList<>();
+            while(wordsIdx<words.length && length+words[wordsIdx].length()<=maxWidth){
+                    temp.add(words[wordsIdx]);
+                    length+=words[wordsIdx].length()+1;
+                    width-=words[wordsIdx].length();
+                    wordsIdx++;
             }
 
+            int spaces=width;
+            int totalWordsInAline=temp.size();
+            int space=totalWordsInAline>1?spaces/(totalWordsInAline-1):0;
+            StringBuilder stringBuilder= new StringBuilder();
+            for (int i = 0; i < totalWordsInAline; i++) {
+                String word = temp.get(i);
+                stringBuilder.append(word);
+
+                if(totalWordsInAline>1 &&spaces>0){
+                    for (int j = 0; j < space; j++) {
+                        stringBuilder.append(" ");
+                        spaces--;
+                    }
+
+
+                }
+
+                if(i==totalWordsInAline-1){
+                    while (spaces>0){
+                        stringBuilder.append(" ");
+                        spaces--;
+                    }
+                }
+
+            }
+            list.add(stringBuilder.toString());
 
 
 
@@ -25,7 +53,7 @@ public class TextJustification {
     }
 
     public static void main(String[] args) {
-        String[] words = {"This", "is", "an", "example", "of", "text", "justification."};
+        String[] words = {"What","must","be","acknowledgment","shall","be"};
         int maxWidth = 16;
         TextJustification textJustification= new TextJustification();
         List<String> result = textJustification.fullJustify(words, maxWidth);
