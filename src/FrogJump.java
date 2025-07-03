b3546a6e86ed156dfcc9d71c5cf2ae141e1fd5f3
harsh4056 <hs4056@gmail.com>
2025-07-03 20:11:39 +0530

    question

--- src/FrogJump.java
+++ src/FrogJump.java
@@ -1,34 +1,61 @@
+import java.util.HashSet;
+
 public class FrogJump {
 
     public boolean canCross(int[] stones) {
 
-        boolean[] stonesVisited= new boolean[stones.length];
-        solveCross(stones,stonesVisited,1,0);
-        return stonesVisited[stones.length-1];
-    }
-
-    public void solveCross(int[]stones,boolean[]stonesVisited,int index,int jump){
-        if(index>=stones.length){
-            return;
+        HashSet<Integer> stonesSet= new HashSet<>();
+        for (Integer integer : stones) {
+            stonesSet.add(integer);
         }
 
+        int[][]memo= new int[stones.length][stones.length];
+        for (int i = 0; i < memo.length; i++) {
 
-            for (int i = jump-1; i<=jump+1 ; i++) {
-                int currentStone=0;
-                for(int j=index;j<stones.length;j++) {
-                    currentStone = stones[j];
-                    int possibleJump = (i + stones[index - 1]);
-                    if (currentStone == possibleJump) {
-                        stonesVisited[index] = true;
-                        solveCross(stones, stonesVisited, index + 1, i);
-                    }
-                    if(possibleJump<stones[j]){
-                        break;
-                    }
-                }
+            for (int j = 0; j < memo[0].length; j++) {
+
+                
             }
+        }
 
+        return solveCross(stonesSet, 0,0,stones[stones.length-1]);
+        //return true;
     }
+
+    public boolean solveCross(HashSet<Integer> set, int value,int jump, int lastValue){
+        if (value == lastValue) {
+            return true;
+        }
+            if(set.contains(value)) {
+                set.remove(value);
+                for (int i = jump - 1; i <= jump + 1; i++) {
+                    if (solveCross(set, value+i,i, lastValue))
+                        return true;
+                }
+                set.add(value);
+            }
+            return false;
+    }
+
+   /* public boolean solveCross(HashSet<Integer> set, int value, int jump, int lastValue){
+        if(value==lastValue){
+            return true;
+        }
+
+        if(set.contains(value)){
+            set.remove(value);
+            if(
+                    solveCross(set, value+jump-1,jump-1,lastValue)||
+                            solveCross(set, value+jump,jump,lastValue)||
+                            solveCross(set, value+jump+1,jump+1,lastValue))
+                return true;
+            set.add(value);
+        }
+        return false;
+
+
+
+    }*/
 
     public static void main(String[] args) {
         FrogJump frogJump = new FrogJump();
@@ -38,8 +65,12 @@ public class FrogJump {
 
         int[] stones2 = {0,1,3,6,7};
         System.out.println(frogJump.canCross(stones2)); // Expected: false
+        int[] stones3 = new int[500];
+        for(int i=0;i<500;i++){
+            stones3[i]=i;
+       }
+        //stones3[stones3.length-1]=999;
 
-        int[] stones3 = {0,1,2,3,4,5,6,7,8,9,10};
         System.out.println(frogJump.canCross(stones3)); // Expected: true
     }
 
