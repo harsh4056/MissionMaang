7d1bbfa54e39327343ed046f99ec497bc5fc9666
harsh4056 <hs4056@gmail.com>
2025-10-14 02:08:23 +0530

    questions

--- src/DeleteAndEarn.java
+++ src/DeleteAndEarn.java
@@ -1,36 +1,48 @@
-import java.util.Arrays;
+import java.util.*;
+
 
 public class DeleteAndEarn {
 
     public int deleteAndEarn(int[] nums) {
         Arrays.sort(nums);
-        int[] dp = new int[nums.length + 1];
-        Arrays.fill(dp, -1);
-        int max= getMax(nums, 0, -2, dp);
-        return max;
-    }
-
-    public int getMax(int[] nums, int index, int prev, int[] dp) {
-        if (index >= nums.length)
-            return 0;
-        if (dp[index] != -1)
-            return dp[index];
-        int take;
-        if (prev + 1 != nums[index]) {
-            take = nums[index] + getMax(nums, index + 1, nums[index], dp);
-        } else {
-            take = getMax(nums, index + 1, prev, dp);
+        int n = nums.length;
+        int[][] dp = new int[n][2];
+        HashMap<Integer,Integer> map= new HashMap<>();
+        List<Integer> list= new ArrayList<>();
+        for(int num:nums) {
+            if(!map.containsKey(num)) list.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        int dontTake = getMax(nums, index + 1, -2, dp);
-        int max = Math.max(take, dontTake);
-        dp[index]=Math.max(dp[index],max);
-        return dp[index];
+        Collections.sort(list);
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
 
+        return getMax(list, 0, 0,map, dp);
     }
+
+    public int getMax(List<Integer> nums, int index, int prevTaken,HashMap<Integer,Integer> map, int[][] dp) {
+        if (index >= nums.size()) return 0;
+        if (dp[index][prevTaken] != -1) return dp[index][prevTaken];
+
+        // Check if current is adjacent to previous index
+        boolean adjacentToPrev = index > 0 && nums.get(index) ==  nums.get(index - 1) + 1;
+
+        int take = 0;
+        // We can take current only if previous index wasn't taken OR they aren't adjacent
+        if (!(prevTaken == 1 && adjacentToPrev)) {
+            take = nums.get(index)*map.get(nums.get(index)) + getMax(nums, index + 1, 1,map, dp);
+        }
+
+        int dontTake = getMax(nums, index + 1, 0,map, dp);
+
+        dp[index][prevTaken] = Math.max(take, dontTake);
+        return dp[index][prevTaken];
+    }
+
+
 
     public static void main(String[] args) {
         DeleteAndEarn soln= new DeleteAndEarn();
 
-        System.out.println(soln.deleteAndEarn(new int[]{2,3}));
+        System.out.println(soln.deleteAndEarn(new int[]{2,2,3,3,4,5}));
     }
 }
--- src/MaximumTotalDamageWithSpellCasting.java
+++ src/MaximumTotalDamageWithSpellCasting.java
@@ -1,42 +1,47 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class MaximumTotalDamageWithSpellCasting {
 
     public long maximumTotalDamage(int[] power) {
         Arrays.sort(power);
         int n = power.length;
-        long[] dp = new long[n];
-        long max = 0;
-
-        for (int i = 0; i < n; i++) {
-            int last = lastIndex(power, i);
-            dp[i] = power[i] + (last != -1 ? dp[last] : 0);
-            if (i > 0) dp[i] = Math.max(dp[i], dp[i - 1]); // choose max till i
-            max = Math.max(max, dp[i]);
+        long[][] dp = new long[n][2];
+        HashMap<Integer,Integer> map= new HashMap<>();
+        List<Integer> list= new ArrayList<>();
+        for(int num:power) {
+            if(!map.containsKey(num)) list.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        return max;
+        Collections.sort(list);
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        return getMax(list, 0, 0,map, dp);
 
     }
-    public int lastIndex(int[]power,int index){
-        int num = power[index];
-        int left = 0, right = index - 1, ans = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (power[mid] < num - 2) {
-                ans = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return ans;
+    public long getMax(List<Integer> nums, int index, int prevTaken, HashMap<Integer,Integer> map, long[][] dp) {
+        if (index >= nums.size()) return 0;
+        if (dp[index][prevTaken] != -1) return dp[index][prevTaken];
 
+        // Check if current is adjacent to previous index
+        boolean adjacentToPrev = index > 0 && ((nums.get(index) ==  (nums.get(index - 1) + 1))||(nums.get(index) ==  (nums.get(index - 1) + 2)));
+        boolean adjacentToPrevPrev = index > 1 && ((nums.get(index) ==  nums.get(index - 2) + 1)||(nums.get(index) ==  (nums.get(index - 2) + 2)));
+
+        long take = 0;
+        // We can take current only if previous index wasn't taken OR they aren't adjacent
+        if (!(prevTaken == 1 && (adjacentToPrev)||adjacentToPrevPrev)) {
+            take = nums.get(index)*(long)map.get(nums.get(index)) + getMax(nums, index + 1, 1,map, dp);
+        }
+
+        long dontTake = getMax(nums, index + 1, 0,map, dp);
+
+        dp[index][prevTaken] = Math.max(take, dontTake);
+        return dp[index][prevTaken];
     }
 
     public static void main(String[] args) {
         MaximumTotalDamageWithSpellCasting soln= new MaximumTotalDamageWithSpellCasting();
-        System.out.println(soln.maximumTotalDamage(new int[]{1,1,1,1,1,1,4,3,6}));//12
-        System.out.println(soln.maximumTotalDamage(new int[]{7,1,6,6}));//13
+        /*System.out.println(soln.maximumTotalDamage(new int[]{1,1,1,1,1,1,4,3,6}));//12
+        System.out.println(soln.maximumTotalDamage(new int[]{7,1,6,6}));//13*/
         System.out.println(soln.maximumTotalDamage(new int[]{5,9,2,10,2,7,10,9,3,8}));//31
     }
 }
